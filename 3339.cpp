3f92a9501a5672f8d0f60653a0268d91990ea42c
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2020-01-13 23:22:07 +0800

    Update 3339.cpp

--- 3339.cpp
+++ 3339.cpp
@@ -45,63 +45,73 @@ int get_time(char s[]){
 }
 
 int main(){
-    char str[500];
-    int D=0,M=0,S=0,G=0,i;
-    for(i=0;i<22;i++){
-        scanf("%d %s %c %[^\n]",&p[i].number,p[i].name,&p[i].role,str);
-        p[i].time=get_time(str);
-        if(p[i].role == 'D')D++;
-        else if(p[i].role == 'M')M++;
-        else if(p[i].role == 'S')S++;
-        else G++;
-    }
-    char team[5];
-    scanf("%s",team);
-    scanf("%d",&i);
-    int g=1,d=team[0]-'0',m=team[2]-'0',s=team[4]-'0';
-    if(G<g || D<d || M<m || S<s){
-        printf("IMPOSSIBLE TO ARRANGE\n\n");
-        return 0;
-    }
-    sort(p,p+22,cmpnumber);
-    D=d,M=m,S=s,G=g;
-    for(i=0;i<22;i++){
-        if(G && p[i].role == 'G'){
-            G--;
-            p[i].flag=12-g+G;
-        }
-        else if(D && p[i].role == 'D'){
-            D--;
-            p[i].flag=12-g-d+D;
-        }
-        else if(M && p[i].role == 'M'){
-            M--;
-            p[i].flag=12-g-d-m+M;
-        }
-        else if(S && p[i].role == 'S'){
-            S--;
-            p[i].flag=12-g-d-m-s+S;
-        }
-        else p[i].flag=0;
-    }
-    //captain
-    int max_time=-1,max_time_number;
-    for(i=0;i<22;i++){
-        if(p[i].flag){
-            if(p[i].time>max_time){
-                max_time=p[i].time;
-                max_time_number=i;
-            }
-        }
-    }
-    p[max_time_number].flag=12;
+	int i;
+	while(scanf("%d",&p[0].number)!=EOF){
+		char str[500];
+		int D=0,M=0,S=0,G=0;
+		scanf("%s %c %[^\n]",p[0].name,&p[0].role,str);
+		p[0].time=get_time(str);
+		if(p[0].role == 'D')D++;
+		else if(p[0].role == 'M')M++;
+		else if(p[0].role == 'S')S++;
+		else G++;
+		
+		for(i=1;i<22;i++){
+			scanf("%d %s %c %[^\n]",&p[i].number,p[i].name,&p[i].role,str);
+			p[i].time=get_time(str);
+			if(p[i].role == 'D')D++;
+			else if(p[i].role == 'M')M++;
+			else if(p[i].role == 'S')S++;
+			else G++;
+		}
+		char team[10];
+		scanf("%s",team);
+		scanf("%d",&i);
+		int g=1,d=team[0]-'0',m=team[2]-'0',s=team[4]-'0';
+		if(G<g || D<d || M<m || S<s){
+			printf("IMPOSSIBLE TO ARRANGE\n\n");
+			return 0;
+		}
+		sort(p,p+22,cmpnumber);
+		D=d,M=m,S=s,G=g;
+		for(i=0;i<22;i++){
+			if(G && p[i].role == 'G'){
+				G--;
+				p[i].flag=12-g+G;
+			}
+			else if(D && p[i].role == 'D'){
+				D--;
+				p[i].flag=12-g-d+D;
+			}
+			else if(M && p[i].role == 'M'){
+				M--;
+				p[i].flag=12-g-d-m+M;
+			}
+			else if(S && p[i].role == 'S'){
+				S--;
+				p[i].flag=12-g-d-m-s+S;
+			}
+			else p[i].flag=0;
+		}
+		//captain
+		int max_time=-1,max_time_number;
+		for(i=0;i<22;i++){
+			if(p[i].flag){
+				if(p[i].time>max_time){
+					max_time=p[i].time;
+					max_time_number=i;
+				}
+			}
+		}
+		p[max_time_number].flag=12;
 
-    sort(p,p+22,cmpflag);
-    for(i=0;i<22;i++){
-        if(p[i].flag != 0){
-            printf("%d %s %c\n",p[i].number,p[i].name,p[i].role);
-        }
-    }
-    printf("\n");
+		sort(p,p+22,cmpflag);
+		for(i=0;i<22;i++){
+			if(p[i].flag != 0){
+				printf("%d %s %c\n",p[i].number,p[i].name,p[i].role);
+			}
+		}
+		printf("\n");
+	}
     return 0;
 }
