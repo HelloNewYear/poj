25e095454ed6d6a282f7d5bdb003a2fb2f141110
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2020-01-14 19:41:13 +0800

    Update 3650.cpp

--- 3650.cpp
+++ 3650.cpp
@@ -1,21 +1,22 @@
 #include<iostream>
+#include<cstdio>
 using namespace std;
 
 int main(){
     char c;
     c=getchar();
-	while(c!='#'){
-		switch(c){
-			case ' ':cout<<"%20";break;
-			case '!':cout<<"%21";break;
-			case '$':cout<<"%24";break;
-			case '%':cout<<"%25";break;
-			case '(':cout<<"%28";break;
-			case ')':cout<<"%29";break;
-			case '*':cout<<"%2a";break;
-			default :cout<<c;
-		}
-		c=getchar();
-	}
-	return 0;
+    while(c!='#'){
+        switch(c){
+            case ' ':cout<<"%20";break;
+            case '!':cout<<"%21";break;
+            case '$':cout<<"%24";break;
+            case '%':cout<<"%25";break;
+            case '(':cout<<"%28";break;
+            case ')':cout<<"%29";break;
+            case '*':cout<<"%2a";break;
+            default :cout<<c;
+        }
+        c=getchar();
+    }
+    return 0;
 }
