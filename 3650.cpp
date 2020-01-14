eb76ad76b7d5de4265217679c0006301b2a57aaf
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2020-01-14 20:12:21 +0800

    Update 3650.cpp

--- 3650.cpp
+++ 3650.cpp
@@ -20,3 +20,29 @@ int main(){
     }
     return 0;
 }
+
+/**
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+using namespace std;
+
+int main(){
+    char str[500];
+    scanf("%[^#]",str);
+    int len=strlen(str);
+    for(int i=0;i<len;i++){
+        switch(str[i]){
+            case ' ':cout<<"%20";break;
+            case '!':cout<<"%21";break;
+            case '$':cout<<"%24";break;
+            case '%':cout<<"%25";break;
+            case '(':cout<<"%28";break;
+            case ')':cout<<"%29";break;
+            case '*':cout<<"%2a";break;
+            default :cout<<str[i];
+        }
+    }
+    return 0;
+}
+**/
