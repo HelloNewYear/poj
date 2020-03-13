a6619e37f9dac9d364aea5ff70f080704c2d1b21
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2020-03-13 18:47:26 +0800

    Update 3456.cpp

--- 3456.cpp
+++ 3456.cpp
@@ -2,30 +2,27 @@
 using namespace std;
 
 int main(){
-    int a,b,c,d,w,x,y,z;
-    long s;
+    int a,b,c,d;
+    a=8;b=5;c=9;d=7;
+    long s,w,x,y,z;
     int f[1000001];
     for(long i=0;i<1000001;i++)f[i]=0;
-    a=8;b=5;c=9;d=7;
     for(w=0;1;w++){
         for(x=0;1;x++){
             for(y=0;1;y++){
                 for(z=0;1;z++){
                     s=a*w+b*x+c*y+d*z;
-                    if(s>1000000){z=0;break;}
-                    else f[s]=1;
+                    if(s<1000001)f[s]=1;
+                    else{z=0;break;}
                 }
                 s=a*w+b*x+c*y+d*z;
                 if(s>1000000){y=0;z=0;break;}
-                else f[s]=1;
             }
             s=a*w+b*x+c*y+d*z;
             if(s>1000000){x=0;y=0;z=0;break;}
-            else f[s]=1;
         }
         s=a*w+b*x+c*y+d*z;
         if(s>1000000)break;
-        else f[s]=1;        
     }
     s=0;
     for(long i=0;i<1000001;i++)s+=f[i];
