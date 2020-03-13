fee4adcc619fc26f8c1da4e67b8c22de3364ee39
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2020-03-13 19:43:11 +0800

    Update 3456.cpp

--- 3456.cpp
+++ 3456.cpp
@@ -1,31 +1,57 @@
 #include <iostream>
 using namespace std;
 
+long F(long x, long y){
+    if(x==y)return 1000000;
+    else return x*y-x-y;
+}
+
+long min(long a, long b, long c, long d, long e, long f){
+    long g=a;
+    if(g>b)g=b;
+    if(g>c)g=c;
+    if(g>d)g=d;
+    if(g>e)g=e;
+    if(g>f)g=f;
+    if(g<1000000)return g;
+    else return 1000000;
+}
+
 int main(){
-    int a,b,c,d;
+    long a,b,c,d;//输入的4个数
+    long m;//最小的不能表示的数
     a=8;b=5;c=9;d=7;
-    long s,w,x,y,z;
-    int f[1000001];
-    for(long i=0;i<1000001;i++)f[i]=0;
+    a=5;b=8;c=5;d=5;
+    a=1938;b=1939;c=1940;d=1937;
+    m=min(F(a,b),F(a,c),F(a,d),F(b,c),F(b,d),F(c,d));
+    long f[m+1],i;//循环变量
+    for(i=0;i<m+1;i++)f[i]=0;
+    long w,x,y,z;//系数
+    long s;//和
     for(w=0;1;w++){
         for(x=0;1;x++){
             for(y=0;1;y++){
                 for(z=0;1;z++){
                     s=a*w+b*x+c*y+d*z;
-                    if(s<1000001)f[s]=1;
-                    else{z=0;break;}
+                    if(s>m){z=0;break;}
+                    else f[s]=1;
                 }
                 s=a*w+b*x+c*y+d*z;
-                if(s>1000000){y=0;z=0;break;}
+                if(s>m){y=0;z=0;break;}
             }
             s=a*w+b*x+c*y+d*z;
-            if(s>1000000){x=0;y=0;z=0;break;}
+            if(s>m){x=0;y=0;z=0;break;}
         }
         s=a*w+b*x+c*y+d*z;
-        if(s>1000000)break;
+        if(s>m)break;
     }
     s=0;
-    for(long i=0;i<1000001;i++)s+=f[i];
-    cout<<1000001-s;
+    for(i=1;i<m+1;i++)s+=f[i];
+    cout<<m-s<<endl;
+    i=m;
+    while(i){
+        if(f[i]!=1){cout<<i;break;}
+        i--;
+    }
     return 0;
 }
