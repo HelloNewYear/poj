8ecd7279b5e0047adcc5068d53747c0af16c57e1
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2020-03-14 10:11:32 +0800

    Update 3456.cpp

--- 3456.cpp
+++ 3456.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int a,b,c,d;
+bool f[1000001];
+
+bool judge(long i){
+    if(i<0)return false;
+    if(f[i]==true)return true;
+    if(i==0||i==a||i==b||i==c||i==d){f[i]=true;return true;}
+    f[i]=judge(i-a)|judge(i-b)|judge(i-c)|judge(i-d);return f[i];
+}
+
 int main(){
-    int a,b,c,d;
-    a=8;b=5;c=9;d=7;
-    long s,w,x,y,z;
-    int f[1000001];
-    for(long i=0;i<1000001;i++)f[i]=0;
-    for(w=0;1;w++){
-        for(x=0;1;x++){
-            for(y=0;1;y++){
-                for(z=0;1;z++){
-                    s=a*w+b*x+c*y+d*z;
-                    if(s<1000001)f[s]=1;
-                    else{z=0;break;}
-                }
-                s=a*w+b*x+c*y+d*z;
-                if(s>1000000){y=0;z=0;break;}
+    long n,m,s,i;
+    cin>>n;
+    while(n--){
+        cin>>a>>b>>c>>d;
+        m=1000000;
+        for(i=1;i<m+1;i++)f[i]=false;
+        s=0;
+        for(i=m;i>0;i--)
+            if(f[i]!=true){
+                judge(i);
+                if(f[i]!=true)s++;
             }
-            s=a*w+b*x+c*y+d*z;
-            if(s>1000000){x=0;y=0;z=0;break;}
-        }
-        s=a*w+b*x+c*y+d*z;
-        if(s>1000000)break;
+        cout<<s<<endl;
+        m=2*s-1;
+        if(m<=1000000)cout<<m<<endl;
+        else cout<<-1<<endl;
     }
-    s=0;
-    for(long i=0;i<1000001;i++)s+=f[i];
-    cout<<1000001-s;
     return 0;
 }
