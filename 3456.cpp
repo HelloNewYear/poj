191f0d2155a0950a528be3b962d6126ab8784779
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2020-03-14 17:43:40 +0800

    Update 3456.cpp

--- 3456.cpp
+++ 3456.cpp
@@ -1,48 +1,23 @@
-#include <iostream>
-using namespace std;
+#include<stdio.h>
+#include<string.h>
 
-int a,b,c,d;
-bool f[1000001];
-
-long F(long x, long y){
-    if(x==y)return 1000000;
-    else return x*y-x-y;
-}
-
-long min(long a, long b, long c, long d, long e, long f){
-    long g=a;
-    if(g>b)g=b;
-    if(g>c)g=c;
-    if(g>d)g=d;
-    if(g>e)g=e;
-    if(g>f)g=f;
-    if(g<1000000)return g;
-    else return 1000000;
-}
-
-bool judge(long i){
-    if(i<0)return false;
-    if(i==0||i==a||i==b||i==c||i==d){f[i]=true;return true;}
-    f[i]=judge(i-a)|judge(i-b)|judge(i-c)|judge(i-d);return f[i];
-}
-
+int a,b,c,d,f[2000002];
 int main(){
     long n,m,s,i;
-    cin>>n;
+    scanf("%ld",&n);
     while(n--){
-        cin>>a>>b>>c>>d;
-        m=min(F(a,b),F(a,c),F(a,d),F(b,c),F(b,d),F(c,d));
-        for(i=1;i<m+1;i++)f[i]=false;
+        scanf("%d %d %d %d",&a,&b,&c,&d);
+        memset(f,0,sizeof(f));
+        f[0]=1;
         s=0;
-        for(i=m;i>0;i--)
-            if(!f[i]){
-                judge(i);
-                if(!f[i])s++;
-            }
-        cout<<s<<endl;
-        m=2*s-1;
-        if(m<=1000000)cout<<m<<endl;
-        else cout<<-1<<endl;
+        m=-1;
+        for(i=0;i<1050001;i++){
+            if(i<=1000000&&!f[i])s++;
+            if(f[i])f[i+a]=f[i+b]=f[i+c]=f[i+d]=1;
+            else m=i;
+        }
+        printf("%ld\n%ld",s,m>1000000?-1:m);
+        if(n!=0)printf("\n");
     }
     return 0;
 }
