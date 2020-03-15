bdd4ac96021cfc6faf527fe948f7c0c65d52d4de
HelloNewYear <36616159+HelloNewYear@users.noreply.github.com>
2020-03-15 16:09:55 +0800

    Update 1664.c

--- 1664.c
+++ 1664.c
@@ -4,12 +4,10 @@ int main(){
     int a[11][11],t,m,n;
     for(t=1;t<=10;t++)a[t][0]=0;
     a[0][0]=1;
-    for(m=0;m<=10;m++){
-        for(n=1;n<=10;n++){
+    for(m=0;m<=10;m++)
+        for(n=1;n<=10;n++)
             if(m<n)a[m][n]=a[m][m];
             else a[m][n]=a[m][n-1]+a[m-n][n];
-        }
-    }
     scanf("%d",&t);
     while(t--){
         scanf("%d %d",&m,&n);
